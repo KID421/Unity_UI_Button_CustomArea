80bacda757e843af35fdaafbf2118452b15f9560
KID <50666207+KID421@users.noreply.github.com>
2020-07-02 15:39:37 +0800

    網頁版

---
+++ CustomArea/Assets/ButtonCustomArea.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCustomArea : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 0.5f;
+    }
+}
